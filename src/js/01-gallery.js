5a3d4c6cda226a68a6625d070c18c98225112d4b
TetianaKolochko <92599848+TetianaKolochko@users.noreply.github.com>
2022-05-08 19:40:49 +0300

    hw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+const cardMarkup = createGallery(galleryItems);
+
+galleryContainer.insertAdjacentHTML('afterbegin', cardMarkup); 
+
+function createGallery(galleryItems) {
+          return galleryItems.map(({ preview, original, description }) => {
+          return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source= "${original}"
+      alt="" title="${description}"
+    />
+  </a>
+</div>`;
+        
+ }).join('');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  
+  captionDelay: 250,
+});
+
+
+  
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,53 @@
+import throttle from "lodash.throttle";
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('.feedback-form input'),
+    message: document.querySelector('.feedback-form textarea'),
+};
+
+let formData = {
+    email: '',
+    message: '',
+
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onTextareaInput, 500));
+
+populateTextarea();
+
+function onTextareaInput(e) {
+    
+    formData [e.target.name] = e.target.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+    
+};
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    e.currentTarget.reset();
+    localStorage.removeItem('feedback-form-state');
+    console.log(formData);
+
+    formData.email = '';
+    formData.message = '';
+}
+
+
+function populateTextarea(e) {
+    const savedMessege = JSON.parse(localStorage.getItem('feedback-form-state'));
+   
+    if (savedMessege) {
+        refs.email.value = savedMessege.email;
+        refs.message.value = savedMessege.message;
+       
+        formData.email = savedMessege.email;
+        formData.message = savedMessege.message;
+       
+    }
+};
+    populateTextarea();
+
+
+
 
